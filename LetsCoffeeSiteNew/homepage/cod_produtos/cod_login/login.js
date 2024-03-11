f25a441f8089530447bdce931b3e6468e54d01e9
joão luccas <joaoluccas637@gmail.com>
2024-03-10 23:16:31 -0300

    novas mudanças

--- LetsCoffeeSiteNew/homepage/cod_produtos/cod_login/login.js
+++ LetsCoffeeSiteNew/homepage/cod_produtos/cod_login/login.js
@@ -1,24 +1,23 @@
 function authenticateUser() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-  
+
     // Defina os logins e senhas pré-definidos
     var clienteLogin = 'cliente';
     var clienteSenha = 'senha_cliente';
     var comercianteLogin = 'comerciante';
     var comercianteSenha = 'senha_comerciante';
-  
+
     if (username === clienteLogin && password === clienteSenha) {
-        window.location.href = 'index.html'; // Redireciona para a interface do cliente
+        window.location.href = '../cod_gerenciament/dashboard.html'; // Redireciona para a interface do cliente
     } else if (username === comercianteLogin && password === comercianteSenha) {
-        window.location.href = 'LetsCoffeeSiteNew\homepage\cod_gerenciamento\dashboard.html'; // Redireciona para a interface do comerciante
+        window.location.href = '../cod_gerenciament/dashboard.html'; // Redireciona para a interface do comerciante
     } else {
         alert('Usuário ou senha incorretos. Tente novamente.');
     }
-  }
+}
 
-  
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     var darkModeToggle = document.getElementById('darkModeToggle');
     var body = document.body;
     var modoEscuro = document.querySelectorAll('.logo');
