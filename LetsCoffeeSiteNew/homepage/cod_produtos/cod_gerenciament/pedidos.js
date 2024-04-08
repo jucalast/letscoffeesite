515f8e0a041574c19a521566c6f379eeae08d73f
joão Luccas <joaoluccas637@gmail.com>
2024-04-08 12:33:01 -0300

    pedidos page

--- LetsCoffeeSiteNew/homepage/cod_produtos/cod_gerenciament/pedidos.js
+++ LetsCoffeeSiteNew/homepage/cod_produtos/cod_gerenciament/pedidos.js
@@ -1,21 +1,22 @@
 var darkModeToggle = document.getElementById('darkModeToggle');
 var body = document.body;
-var modoEscuro = document.querySelectorAll('.logo');
 
 darkModeToggle.addEventListener('change', function () {
     body.classList.toggle('dark-mode', darkModeToggle.checked);
-
-    modoEscuro.forEach(function(element) {
-      element.classList.toggle('darkModeToggle', darkModeToggle.checked);
-    });
-
     localStorage.setItem('darkMode', darkModeToggle.checked ? 'enabled' : 'disabled');
+
+    // Adicione uma classe adicional ao aside quando o modo escuro estiver ativado
+    var aside = document.getElementById('aside');
+    aside.classList.toggle('dark-mode-aside', darkModeToggle.checked);
 });
 
 if (localStorage.getItem('darkMode') === 'enabled') {
     darkModeToggle.checked = true;
     body.classList.add('dark-mode');
-    modoEscuro.forEach(function (element) {
-      element.classList.add('dark-mode-element');
-    });
+
+    // Verifique se o modo escuro está ativado e adicione a classe adicional ao aside, se necessário
+    var aside = document.getElementById('aside');
+    if (aside) {
+        aside.classList.add('dark-mode-aside');
+    }
 }
