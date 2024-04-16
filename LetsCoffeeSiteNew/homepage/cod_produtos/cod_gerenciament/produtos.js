d7c12d553f87393aca72980587c23aa39a1b8b7e
joão Luccas <joaoluccas637@gmail.com>
2024-04-16 15:35:34 -0300

    criação do modal

--- LetsCoffeeSiteNew/homepage/cod_produtos/cod_gerenciament/produtos.js
+++ LetsCoffeeSiteNew/homepage/cod_produtos/cod_gerenciament/produtos.js
@@ -32,5 +32,75 @@ $(document).ready(function() {
     highlightCurrentPage(); // Chama a função ao carregar a página
 });
 
+// Configurações do Dropzone.js
+Dropzone.autoDiscover = false;
 
+document.addEventListener('DOMContentLoaded', function() {
+    var myDropzone = new Dropzone("#myDropzone", {
+        url: "/file-upload",
+        addRemoveLinks: true,
+        dictRemoveFile: '',
+        dictCancelUpload: 'Cancelar upload'
+    });
 
+    myDropzone.on("addedfile", function(file) {
+        // Criar e adicionar o ícone "X" para cada imagem adicionada
+        var deleteIcon = Dropzone.createElement('<span class="delete-icon">&times;</span>');
+        file.previewElement.appendChild(deleteIcon);
+
+        // Adicionar um evento de clique para o ícone "X"
+        deleteIcon.addEventListener('click', function() {
+            // Remover a imagem quando o ícone "X" é clicado
+            myDropzone.removeFile(file);
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var customFieldsCounter = 0;
+
+    document.getElementById('addcustomfield').addEventListener('click', function() {
+        customFieldsCounter++;
+
+        var customField = document.createElement('div');
+        customField.className = 'form-group custom-field';
+        customField.innerHTML = `
+            <div>
+            <div class="remove">
+                <label for="customfield${customFieldsCounter}">campo personalizado ${customFieldsCounter}</label>
+                <button type="button" class="btn btn-danger remove-custom-field">-</button>
+                </div>
+                <input type="text" class="form-control" id="customfield${customFieldsCounter}" placeholder="digite o valor do campo personalizado">
+                
+            </div>
+        `;
+
+        document.querySelector('.formulario form').insertBefore(customField, document.getElementById('addcustomfield'));
+    });
+
+    document.getElementById('productcategory').addEventListener('keydown', function(e) {
+        // Verifica se a tecla pressionada foi Enter
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            // Cria uma nova tag de categoria
+            var categoryTag = document.createElement('span');
+            categoryTag.className = 'category-tag';
+            categoryTag.textContent = this.value;
+
+            // Adiciona a tag de categoria ao DOM
+            document.getElementById('categorytags').appendChild(categoryTag);
+
+            // Limpa o campo de entrada
+            this.value = '';
+        }
+    });
+
+    // Evento de clique para remover o campo personalizado
+    document.querySelector('.formulario form').addEventListener('click', function(e) {
+        if (e.target.classList.contains('remove-custom-field')) {
+            // Remove todo o grupo de campo personalizado
+            e.target.parentElement.parentElement.remove();
+        }
+    });
+});
